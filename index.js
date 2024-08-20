613c6e3a20af535a4e4ead6ca29344581edced65
Your Name <rudrakshg1914@gmail.com>
2024-08-20 12:18:20 +0530

    added cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 const corsOptions = {
     origin: "https://job-portal-dusky-tau.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 };
 
